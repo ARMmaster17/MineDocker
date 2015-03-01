5ebd5e2d2ae5e6a86aacc13f9ecae8179cffb0d9
Joshua Zenn <wordman05@gmail.com>
2015-02-28 20:16:10 -0500

    Update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -1,2 +1,11 @@
-!#bin/bash
-apt-get install docker
+#!bin/bash
+
+# Install dependencies
+sudo apt-get install docker
+sudo apt-get install wget
+sudo apt-get install openjdk-6-jre
+
+# Install minecraft
+mkdir vanilla
+cd vanilla
+wget https://s3.amazonaws.com/Minecraft.Download/versions/1.8.3/minecraft_server.1.8.3.jar
