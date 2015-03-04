8d172ba7dd298cfa04e809f5726a6e5a22958479
Joshua Zenn <wordman05@gmail.com>
2015-03-04 10:38:05 -0500

    Update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -5,7 +5,12 @@ sudo apt-get install docker
 sudo apt-get install wget
 sudo apt-get install openjdk-6-jre
 
+# Switch to root folder
+cd ..
+
 # Install minecraft
 mkdir vanilla
 cd vanilla
 wget https://s3.amazonaws.com/Minecraft.Download/versions/1.8.3/minecraft_server.1.8.3.jar
+
+# Store location in settings file
